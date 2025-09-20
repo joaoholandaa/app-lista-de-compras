00b0a63cb202bbaf7306a319ebf91c3802b282d3
joaoholandaa <joaocmdt505@gmail.com>
2025-09-20 15:21:57 -0300

    feat: adicionar itens ao adaptador

--- app/src/main/java/br/com/holandadev/listadecompras/MainActivity.kt
+++ app/src/main/java/br/com/holandadev/listadecompras/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +17,14 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView);
         val itemsAdapter = ItemsAdapter()
         recyclerView.adapter = itemsAdapter
+
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editText)
+        button.setOnClickListener {
+            val item = ItemModel(
+                name = editText.text.toString()
+            )
+            itemsAdapter.addItem(item)
+        }
     }
 }
