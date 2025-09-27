d2d28024ab961e1c62effbdc1d5ab4ba7f5789f7
joaoholandaa <joaocmdt505@gmail.com>
2025-09-27 01:20:51 -0300

    feat: criar instancia do banco de dados

---
+++ app/src/main/java/br/com/holandadev/listadecompras/ItemsViewModelFactory.kt
@@ -0,0 +1,21 @@
+package br.com.holandadev.listadecompras
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import br.com.holandadev.listadecompras.data.ItemsDatabase
+
+class ItemsViewModelFactory(private val applicationContext: Context): ViewModelProvider.Factory {
+
+    private fun createDatabase(): ItemsDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            ItemsDatabase::class.java, "items_database.db"
+        ).build()
+    }
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return ItemsViewModel(database = createDatabase()) as T
+    }
+}
